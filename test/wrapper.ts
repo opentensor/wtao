aad5f13a0e4a7a605018fa6d99149bcf0f665671
Greg Zaitsev <gregz@opentensor.dev>
2025-05-20 16:40:58 -0400

    Add tests

---
+++ test/wrapper.ts
@@ -0,0 +1,70 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { HardhatEthersProvider } from "@nomicfoundation/hardhat-ethers/internal/hardhat-ethers-provider";
+import { WTAO } from "../typechain-types/contracts/WTAO";
+import BigNumber from 'bignumber.js';
+BigNumber.config({ DECIMAL_PLACES: 0, ROUNDING_MODE: BigNumber.ROUND_DOWN, EXPONENTIAL_AT: 255 });
+
+describe("WTAO contract", function () {
+  let sutContract: WTAO;
+  let gasPrice: BigNumber;
+  let provider: HardhatEthersProvider;
+  let caller: any;
+  let gasParameters: {
+    gasLimit: ethers.BigNumber,
+    maxFeePerGas: ethers.BigNumber,
+    maxPriorityFeePerGas: ethers.BigNumber,
+  } = {
+    gasLimit: 2000000,
+    maxFeePerGas: ethers.parseUnits("10", "gwei"),
+    maxPriorityFeePerGas: ethers.parseUnits("10", "gwei"),
+  };
+
+  before(async () => {
+    provider = ethers.provider;
+    [caller] = await ethers.getSigners();
+    sutContract = await ethers.deployContract("WTAO", gasParameters);
+    await sutContract.waitForDeployment();
+    console.log(`Contract deployed at address: ${await sutContract.getAddress()}`);
+  });
+
+  it("Deposit TAO", async () => {
+    const balanceBefore = await provider.getBalance(caller.address);
+    const tx = await sutContract.deposit({ ...gasParameters, value: ethers.parseEther("0.1") });
+
+    // Wait for the transaction to be mined
+    await tx.wait();
+
+    // Check user WTAO balance
+    const wtaoBalance = await sutContract.balanceOf(caller);
+    expect(wtaoBalance).to.be.eq(ethers.parseEther("0.1"));
+
+    // Check that TAO is stored in the contract
+    const tvlBalance = await sutContract.getContractETHBalance();
+    expect(tvlBalance).to.be.eq(ethers.parseEther("0.1"));
+
+    // Check that user balance decreased
+    const balanceAfter = await provider.getBalance(caller.address);
+    expect(balanceBefore - balanceAfter).to.be.gte(ethers.parseEther("0.1"));
+  });
+
+  it("Withdraw TAO", async () => {
+    const balanceBefore = await provider.getBalance(caller.address);
+    const tx = await sutContract.withdraw(ethers.parseEther("0.1"), { ...gasParameters});
+
+    // Wait for the transaction to be mined
+    await tx.wait();
+
+    // Check user WTAO balance
+    const wtaoBalance = await sutContract.balanceOf(caller);
+    expect(wtaoBalance).to.be.eq(0);
+
+    // Check that TAO is stored in the contract
+    const tvlBalance = await sutContract.getContractETHBalance();
+    expect(tvlBalance).to.be.eq(0);
+
+    // Check that user balance increased
+    const balanceAfter = await provider.getBalance(caller.address);
+    expect(balanceAfter - balanceBefore).to.be.gte(0);
+  });
+});
